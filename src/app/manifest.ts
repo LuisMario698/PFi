a27240b86473826fc99e44be2866a7080c46a887
Luis Mario <LuisMario698@hotmail.com>
2026-07-15 16:26:45 -0700

    feat: add PWA icons and update manifest configuration

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -9,5 +9,19 @@ export default function manifest(): MetadataRoute.Manifest {
     display: 'standalone',
     background_color: '#09090b',
     theme_color: '#09090b',
+    icons: [
+      {
+        src: '/icon-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+        purpose: 'maskable'
+      },
+      {
+        src: '/icon-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'maskable'
+      }
+    ],
   };
 }
